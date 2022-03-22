7c28e1252e2345356a8e6d03103f9372fa0abb00
habtamuadargaso <habtamu.adargaso@gmail.com>
2022-03-21 21:56:22 -0700

    Reverse Given integer and return reverse number

---
+++ miuExamSolution/ReverseGivenInteger.java
@@ -0,0 +1,29 @@
+package miuExamSolution;
+
+
+public class ReverseGivenInteger
+{
+    public static void main(String[] args) {
+        int samplaData = 11235 ; 
+        int result = reverseGivenInteger(samplaData);
+        System.out.println(result);
+    }
+    static int reverseGivenInteger(int givenInteger){
+        int sign = 1; 
+        if (givenInteger == 0){
+            return 0 ; 
+            
+        } 
+        if(givenInteger < 0 ){
+            sign = -1 ;
+            givenInteger = (-1)* givenInteger ;
+        }
+        int reverse = 0;
+        while(givenInteger != 0){
+            reverse = (reverse*10) + (givenInteger%10);
+            givenInteger = givenInteger/10;
+        }
+        return sign * reverse ;
+    }
+    
+}
