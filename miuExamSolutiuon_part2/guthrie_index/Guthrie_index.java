4d248f2c560fec17d92ba7956c758ff20d541567
habtamuadargaso <habtamu.adargaso@gmail.com>
2022-04-09 09:05:49 -0700

    guthrie_index count

--- miuExamSolutiuon_part2/guthrie_index/Guthrie_index.java
+++ miuExamSolutiuon_part2/guthrie_index/Guthrie_index.java
@@ -6,7 +6,6 @@ public class Guthrie_index{
     public static void main (String[] args){
      int result = guthrieindex(1);
      System.out.println(result);
-     System.out.println(result);
      result = guthrieindex(3);
      System.out.println(result);
      result = guthrieindex(4);
@@ -16,15 +15,23 @@ public class Guthrie_index{
 
     }
    static int guthrieindex(int n){
+       /*let asign the variable the count the guthrieindex_num=0*/
        int guthrieindex_num = 0 ;
+       /*let check the number is postive or not because the question says that n > 0*/
        if(n > 0){
+           /*we need while loop because we want to go until our number is get 1 */
            while(n !=1){
+               /*if the number is even then divied the number by two  n = number/2 */
                if(n %2 ==0){
+                   
                    n=n/2;
                }
+
+               /*else  if the number is odd number = n *3 +1 */
                else{
                    n = n * 3 + 1  ;
                }
+               /* then count the loop running */
                guthrieindex_num ++;
            }
           
