de777f8ad9803a89b21004ab5d7227ee488e456f
habtamuadargaso <habtamu.adargaso@gmail.com>
2022-04-13 22:30:47 -0700

    n-upcount

--- miuExamSolution/n_Upcount/miuExamSolution/n_Upcount/n_Upcount.java
+++ miuExamSolution/n_Upcount/miuExamSolution/n_Upcount/n_Upcount.java
@@ -20,16 +20,22 @@ public class n_Upcount {
         System.out.print("Upcount IS : " + result);
 
     }
-
+    
     static int Upcount(int a[], int n) {
+        /*intial partial sum is equal to zero */
         int partialSum = 0;
         int perviousSum = 0;
         int Upcount = 0;
 
         for (int i = 0; i < a.length; i++) {
+            /*update the intial partial sum as perviousSum */
             perviousSum = partialSum;
             partialSum = partialSum + a[i];
+
+            /* if partial sum goes from less than or equal to n and greater 
+            than n during the calculation of the element of the array*/
             if (perviousSum <= n && partialSum > n) {
+                /*if this true count the number by one */
                 Upcount++;
             }
         }
